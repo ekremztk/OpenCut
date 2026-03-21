343536f71bfb58a7e3e565de810bb705246f9c96
ekremztk <ekremztk@icloud.com>
2026-03-21 15:24:21 +0100

    feat: remove landing pages, redirect to editor

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,21 +1,6 @@
-import { Hero } from "@/components/landing/hero";
-import { Header } from "@/components/header";
-import { Footer } from "@/components/footer";
-import type { Metadata } from "next";
-import { SITE_URL } from "@/constants/site-constants";
+import { redirect } from 'next/navigation'
+import { randomUUID } from 'crypto'
 
-export const metadata: Metadata = {
-	alternates: {
-		canonical: SITE_URL,
-	},
-};
-
-export default async function Home() {
-	return (
-		<div>
-			<Header />
-			<Hero />
-			<Footer />
-		</div>
-	);
+export default function Home() {
+  redirect(`/editor/${randomUUID()}`)
 }
